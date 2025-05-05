071ae1c0aad61bfde33465581888d72aafd6757a
JD-SWaresol <juan.delgado@smartwaresol.com>
2025-05-05 11:40:22 -0600

    Rector process command

--- process/rector.sh
+++ process/rector.sh
@@ -2,6 +2,17 @@
 
 composer require rector/rector --dev
 
+
 ## Run from 'bin directory' to creat rector.php file to define paths, rules and sets you want to run on your code
 
 vendor/bin/rector
+
+
+## To see preview of suggested changed, run process command with --dry-run option:
+
+vendor/bin/rector process --dry-run
+
+
+## To make changes happen, run bare command:
+
+vendor/bin/rector process
