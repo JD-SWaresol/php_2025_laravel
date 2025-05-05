cefc0880bf561e0096990b5fffe9676b084b5982
JD-SWaresol <juan.delgado@smartwaresol.com>
2025-05-05 10:13:22 -0600

    Create rector.php config file

--- process/rector.sh
+++ process/rector.sh
@@ -2,6 +2,6 @@
 
 composer require rector/rector --dev
 
-## Run from 'bin directory'
+## Run from 'bin directory' to creat rector.php file to define paths, rules and sets you want to run on your code
 
 vendor/bin/rector
