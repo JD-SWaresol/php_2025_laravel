9ed71f23ce9486fe817b3432112b8ab1e3f7cc82
JD-SWaresol <juan.delgado@smartwaresol.com>
2025-05-06 12:32:25 -0600

    Install PhpStan

---
+++ process/php_stan.sh
@@ -0,0 +1,8 @@
+## Installation
+
+composer require phpstan/phpstan --dev
+
+
+## To let PHPStan analyse your codebase, you have to use the analyse command and point it to the right directories.
+
+vendor/bin/phpstan analyse src tests
