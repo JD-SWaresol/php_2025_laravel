40906895692fe1898c6dfc6c6ffaa3f2f6eba0c9
JD-SWaresol <juan.delgado@smartwaresol.com>
2025-05-06 16:38:26 -0600

    PhpStan Analyse

--- process/php_stan.sh
+++ process/php_stan.sh
@@ -3,6 +3,17 @@
 composer require phpstan/phpstan --dev
 
 
-## To let PHPStan analyse your codebase, you have to use the analyse command and point it to the right directories.
+## To let PHPStan analyse your codebase, you have to use the analyse command and point it to the right directories. Analyses tests folder:
+## A path must be specified!!!
 
-vendor/bin/phpstan analyse src tests
+vendor/bin/phpstan analyse tests
+
+
+## Also you can specify an specific file
+
+vendor/bin/phpstan analyse process/test_files/class_not_found.php
+
+
+## You can get cache version phpstan
+
+vendor/bin/phpstan clear-result-cache --version
