e2ba83edc4ae766ca4fb05b6fb54b3ce7c9e62d8
JD-SWaresol <juan.delgado@smartwaresol.com>
2025-05-09 11:22:48 -0600

    Init Pest instruction

--- process/pest.sh
+++ process/pest.sh
@@ -9,3 +9,10 @@ composer require pestphp/pest --dev --with-all-dependencies
 
 ## Secondly, you'll need to initialize Pest in your current PHP project. This step will create a configuration file named Pest.php 
 ## at the root level of your test suite, which will enable you to fine-tune your test suite later.
+
+./vendor/bin/pest --init
+
+
+## Finally, you can run your tests by executing the pest command.
+
+./vendor/bin/pest
