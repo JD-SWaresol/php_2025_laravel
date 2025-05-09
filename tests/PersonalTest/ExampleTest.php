a176416454b05ac31294b897fdc636c290f8d911
JD-SWaresol <juan.delgado@smartwaresol.com>
2025-05-09 13:37:02 -0600

    Update Personal Test to PEST

--- tests/PersonalTest/ExampleTest.php
+++ tests/PersonalTest/ExampleTest.php
@@ -1,6 +1,17 @@
 <?php
 
-echo 'This is a new test!!!'
+namespace Tests\PersonalTest;
 
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
-?>
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function personal_test_in_use(): void
+    {
+        echo 'This is a new Personal test!!!!'
+    }
+}
